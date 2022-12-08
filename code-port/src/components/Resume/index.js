0e50aabce537fe19955f8c1ad2c8ca87722c4b1e
ldholl <lacy.d.holleman@gmail.com>
2022-12-08 12:30:24 -0500

    added basic structure, started filling in jsx

---
+++ code-port/src/components/Resume/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Resume(){
+    <section>
+        <div>
+            {/* resume thumbnail */}
+        </div>
+        
+        <div>
+            {/* image of resume goes here */}
+
+            {/*  download button goes here */}
+        
+        
+        </div>
+    </section>
+}
+
+export default Resume;
