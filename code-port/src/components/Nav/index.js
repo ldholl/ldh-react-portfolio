02077c15dcb66b8d62857361b0d1a875efdeed4d
ldholl <lacy.d.holleman@gmail.com>
2022-12-09 07:05:41 -0500

    sections render conditionally

--- code-port/src/components/Nav/index.js
+++ code-port/src/components/Nav/index.js
@@ -1,14 +1,26 @@
 import React, {useState} from 'react';
-
+import About from '../About';
+import Contact from '../Contact';
+import Projects from '../Projects';
+import Resume from '../Resume';
 import { capitalizeFirstLetter } from '../../utils/helpers';
 
 
 function Nav(){
     const pages= ['about', 'contact', 'projects', 'resume']
     
+    const [currentPage, setCurrentPage] = useState('about');
+
+    function handlePageChange(newPage){
+        setCurrentPage(newPage);
+        console.log(currentPage);
+    }
+
+
+
     return (
         <div>
-            <p>Text</p>
+
        <header>
             <nav>
                 <div className='nav-wrapper'>
@@ -16,14 +28,19 @@ function Nav(){
                     <ul id='nav-mobile' className='right hide-on-med-and-down'>
                         {pages.map((page) => (
                             <li>
-                                <a href={'#' + page} className='nav-links' key={capitalizeFirstLetter(page)}>{capitalizeFirstLetter(page)}</a>
+                                <a href={'#' + page} className='nav-links' key={page} onClick={() => handlePageChange(page)}>{capitalizeFirstLetter(page)}</a>
                             </li>
                         ))}
                     </ul>
                 </div>
             </nav> 
         </header>
-                  
+        <main>
+        {currentPage === 'about' && <About />}
+        {currentPage === 'contact' && <Contact/>}
+        {currentPage === 'resume' && <Resume />}
+        {currentPage === 'projects' && <Projects />}
+        </main>       
         </div>
     )
 }
